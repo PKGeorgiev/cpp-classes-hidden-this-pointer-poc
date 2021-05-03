89e316281f6e940c39b0a7fcb725de3c49f8bd08
Petar Georgiev <petar@pgeorgiev.com>
2021-05-03 18:48:45 +0300

    Add helper functions and setup main code:  * Add showNames()  * Fix comments and typos  * Add diagnostic output in Person's methods  * Add testing code in main()

--- cpp-classes-hidden-this-pointer-poc/Person.h
+++ cpp-classes-hidden-this-pointer-poc/Person.h
@@ -1,16 +1,24 @@
 #pragma once
 #include <string>
+#include <iostream>
 
 class Person
 {
 public:
+    Person()
+    {
+        name = std::string("Unnamed person @ address ") + std::to_string(size_t(this));
+    }
+
     void setName(std::string name)
     {
+        std::cout << "Setting name '" + name + "' for instance " + std::to_string(size_t(this)) << std::endl;
         this->name = name;
     }
 
     std::string getName()
     {
+        std::cout << "Getting name for instance " + std::to_string(size_t(this)) << std::endl;
         return this->name;
     }
 
--- cpp-classes-hidden-this-pointer-poc/cpp-classes-hidden-this-pointer-poc.cpp
+++ cpp-classes-hidden-this-pointer-poc/cpp-classes-hidden-this-pointer-poc.cpp
@@ -3,11 +3,45 @@
 
 using namespace std;
 
-// Pointrs of type METHOD_PTR can point to any method with following signature: 
-// double Person::methodName(string)
-typedef double(Person::* METHOD_PTR)(string);
+// Pointers of type METHOD_PTR can point to any method with following signature: 
+// void Person::methodName(string)
+typedef void(Person::* METHOD_PTR)(std::string);
+
+// Pointers of type FUNCTION_PTR can point to any function with following signature:
+// void funcName(void*, void*, string)
+// Explanation of the __fastcall trick
+// https://tresp4sser.wordpress.com/2012/10/06/how-to-hook-thiscall-functions/
+typedef void(__fastcall* FUNCTION_PTR)(void* pThis, void* notUsed, string);
+
+// Gets method's address via template and reinterpret_cast trick
+// https://stackoverflow.com/a/52427102
+template<typename M> inline void* GetMethodPointer(M methodPtr)
+{
+    return *reinterpret_cast<void**>(&methodPtr);
+}
+
+void showNames(Person& person1, Person& person2)
+{
+    cout << "p1.name: " << person1.getName() << endl;
+    cout << "p2.name: " << person2.getName() << endl;
+}
 
 int main()
 {
-    
+    void* setNameMethodPtr = GetMethodPointer<METHOD_PTR>(&Person::setName);
+    FUNCTION_PTR setNameFunctionPtr = (FUNCTION_PTR)setNameMethodPtr;
+
+    Person p1, p2;
+
+    // Should display "Unnamed person"
+    showNames(p1, p2);
+
+    // Call setName method via the setNameFunctionPtr pointer
+    // The first parameter must the the address of a conrete instance
+    // It will become the 'this' pointer
+    setNameFunctionPtr(&p1, nullptr, "Person 1");
+    setNameFunctionPtr(&p2, nullptr, "Person 2");
+
+    // Should display "Person 1 and Person 2"
+    showNames(p1, p2);
 }
